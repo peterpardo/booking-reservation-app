6d6e7024c16858870b6678ef6e4b57aa42601ea1
Peter Carl Pardo <peterpardo123@gmail.com>
2022-05-17 19:12:31 +0800

    Second day coding

---
+++ src/components/searchItem/SearchItem.jsx
@@ -0,0 +1,26 @@
+import "./searchItem.css";
+
+const SearchItem = () => {
+  return (
+    <div className="searchItem">
+      <img src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1" alt="" className="siImage"/>
+      <div className="siDesc">
+        <h1 className="siTitle">Tower Street Apartments</h1>
+        <span className="siDistance">500m from center</span>
+        <span className="siTaxiOp">Free airport taxi</span>
+        <span className="siSubtitle">
+          Studio Apartment with Air conditioning
+        </span>
+        <span className="siFeatures">
+          Entire studio • 1 bathroom • 21m² 1 full bed
+        </span>
+        <span className="siCancelOp">Free cancellation </span>
+        <span className="siCancelOpSubtitle">
+          You can cancel later, so lock in this great price today!
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default SearchItem
